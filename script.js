508c4914e4453597451c746e03cc509ccbfc5b8f
Andressa Pereira <andressa@outlook.com>
2022-09-27 12:04:12 -0300

    projeto finalizado

---
+++ script.js
@@ -0,0 +1,17 @@
+const botao = document.getElementById('open_modal')
+botao.addEventListener('click', function(e) {
+  const modal = document.getElementById('bg_modal')
+modal.classList.toggle('open')
+
+
+  // modal.style.display = 'flex'
+  // modal.style.opacity = '1'
+})
+
+const botao_fechar = document.getElementById('close_modal')
+botao_fechar.addEventListener('click', function(e) {
+  const modal = document.getElementById('bg_modal')
+  modal.classList.toggle('open')
+  // modal.style.display = 'none'
+  // modal.style.opacity = '0'
+})
